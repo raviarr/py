48456b11e7a650f436dbe8e7d7cc066a1dbac34d
Ravia <raviarr13@gmail.com>
2024-04-18 11:28:30 +1200

    upload day 5 task

---
+++ day5.py
@@ -0,0 +1,28 @@
+def ravia():
+    print('ravia')
+
+def displayName():
+    first_name = input('Enter the first name:')
+    last_name = input('Enter the last name:')
+
+    print("Full name is:", first_name+' '+ last_name)
+
+displayName()
+
+def Add():
+    A= int(input('Enter the first number'))
+    B= int(input('Enter the second number'))
+    Sum = A+B
+    print('The sum is', Sum)
+
+Add()
+
+def leap():
+    year =int(input("Enter a year"))
+    x = 4
+    if year == x:
+      print("it is a leap year")
+    else:
+        print("it is not leap year")
+
+leap()       
